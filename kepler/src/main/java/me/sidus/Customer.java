b208e5e8e032c8e167c5ffe3da34b85d4ee2d593
wangpeiqi <LogicWang@outlook.com>
2018-06-28 00:13:19 +0800

    add new dir

---
+++ kepler/src/main/java/me/sidus/Customer.java
@@ -0,0 +1,13 @@
+package me.sidus;
+
+/**
+ * Created by page on 05/12/2016.
+ */
+public class Customer {
+  private String a;
+  private int b;
+  Customer(String name, int age){
+    a = name;
+    b = age;
+  }
+}
