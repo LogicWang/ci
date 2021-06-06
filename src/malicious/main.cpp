bdedcdb27687ee3166c485e8ac0072a7ff08f164
logic-pw <mangobada@163.com>
2021-06-06 15:33:15 +0800

    move all code to src

---
+++ src/malicious/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main() {
+    struct x struct z<x(x(x(x(x(x(x(x(x(x(x(x(x(x(x(x(x(y,x(y><y*,x(y*w>v<y*,w,x{};
+
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
